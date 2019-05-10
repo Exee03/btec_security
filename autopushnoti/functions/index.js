f0af8ee2d1499cf6a598a52a1d22c3ec98cea604
Exee03 <45579836+Exee03@users.noreply.github.com>
2019-05-11 04:59:52 +0800

    Cloud Firestore to Realtime Database

--- autopushnoti/functions/index.js
+++ autopushnoti/functions/index.js
@@ -3,52 +3,62 @@ const admin = require('firebase-admin');
 
 admin.initializeApp(functions.config().firebase);
 
-exports.statusTrigger = functions.firestore.document(
-    'status/{statusId}'
+exports.statusDatabaseTrigger = functions.database.ref(
+    '/{userId}'
 ).onWrite((change, context) => {
-    const msgData = change.after.exists ? change.after.data() : null;
-    const before = change.before.data();
+    const msgData = change.after.val();
+    const before = change.before.val();
+    console.log('From : ' + before.status);
+    console.log('To : ' + msgData.status);
 
     if (before.status == msgData.status) {
         console.log('Nothing change!');
         return null;
     }
 
-    admin.firestore().collection('users').get().then((snapshots) => {
-        var tokens = [];
-        
-        if (snapshots.empty) {
-            console.log('No devices');
-            return null;
-        } else {
-            for (var user of snapshots.docs) {
-                if (user.data().uid == msgData.uid) {
-                    tokens.push(user.data().token);
-                } else {
-                    console.log('uid not available in users collection');
-                    return null;
+    if (msgData.status == 'Detected!') {
+        admin.firestore().collection('users').get().then((snapshots) => {
+            var tokens = [];
+            console.log("Check Firestore");
+            if (snapshots.empty) {
+                console.log('No devices');
+                return null;
+            } else {
+                console.log("Get User");
+                for (var user of snapshots.docs) {
+                    if (user.data().uid == msgData.uid) {
+                        tokens.push(user.data().token);
+                        console.log("Get Token");
+                    } else {
+                        console.log('uid not available in users collection');
+                        return null;
+                    }
                 }
+
+                var payload = {
+                    'notification': {
+                        'title': 'Status : ' + msgData.status,
+                        'body': 'An unknown person attempts to enter your office ( ' + msgData.name + ' )',
+                        'sound': 'loudalarm.mp3'
+                    },
+                }
+
+                var options = {
+                    priority: "high",
+                    timeToLive: 60 * 60 * 24,
+                    contentAvailable: true
+                };
+
+                return admin.messaging().sendToDevice(tokens, payload, options).then((response) => {
+                    console.log('Successfully push notifications!');
+                }).catch((err) => {
+                    console.log('Error : ' + err);
+                })
             }
-
-            var payload = {
-                'notification': {
-                    'title': 'Status : ' + msgData.status,
-                    'body': 'An unknown person attempts to enter your office ( ' + msgData.name + ' )',
-                    'sound': 'loudalarm.mp3'
-                },
-            }
-
-            var options = {
-                priority: "high",
-                timeToLive: 60 * 60 * 24,
-                contentAvailable: true
-              };
-
-            return admin.messaging().sendToDevice(tokens, payload, options).then((response) => {
-                console.log(' Succefull push notifications : ' + response.results);
-            }).catch((err) => {
-                console.log('Error : ' + err);
-            })
-        }
-    })
+        })
+    }
+    else {
+        console.log("Status : " + msgData.status);
+        return null;
+    }
 })
