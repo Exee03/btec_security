6c176dba7ef9f41d821f432158bc65f36f059cd0
Exee03 <45579836+Exee03@users.noreply.github.com>
2019-05-21 18:42:50 +0800

    add color

--- lib/utils/custom_colors.dart
+++ lib/utils/custom_colors.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:native_color/native_color.dart';
 
 class CustomColors{
-  static Color background = HexColor('#07070E');
-  static Color front = HexColor('#141428');
-  static Color office = HexColor('#EE362E');
-  static Color attendance = HexColor('#F7A316');
-  static Color history = HexColor('#30c9e8');
+  static Color background = Color.fromRGBO(28, 29, 50, 1);
+  static Color bottomBackground = Color.fromRGBO(255, 241, 217, 1);
+  static Color front = Color.fromRGBO(47, 48, 69, 1);
+  static Color office = Color.fromRGBO(254, 52, 76, 1);
+  static Color attendance = Color.fromRGBO(211, 5, 105, 1);
+  static Color history = Color.fromRGBO(0, 155, 113, 1);
+  static Color present = Color.fromRGBO(135, 203, 84, 1);
+  static Color absent = Color.fromRGBO(237, 73, 78, 1);
 }
