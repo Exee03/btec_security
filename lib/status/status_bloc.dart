1e2c78d3e867874f9ba4d0294534970637c4cf44
Exee03 <45579836+Exee03@users.noreply.github.com>
2019-05-26 18:40:06 +0800

    Create status_bloc.dart

---
+++ lib/status/status_bloc.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+import 'package:bloc/bloc.dart';
+import 'package:btec_security/models/model.dart';
+import 'package:btec_security/repository/status_repo.dart';
+import './bloc.dart';
+
+class StatusBloc extends Bloc<StatusEvent, StatusState> {
+  final StatusRepository statusRepository;
+
+  StatusBloc({this.statusRepository}) : assert(statusRepository != null);
+
+  @override
+  StatusState get initialState => InitialStatusState();
+
+  @override
+  Stream<StatusState> mapEventToState(
+       StatusEvent event) async* {
+    print("mapEventToState");
+    yield StatusLoading();
+    try {
+      List<Status> listStatus;
+      if (event is ListStarted) {
+        listStatus = await statusRepository.fetchStatusByDay(event.user.uid);
+        print(listStatus);
+      }
+      if (listStatus.length == 0) {
+        yield StatusEmpty();
+      } else {
+        yield StatusLoaded(status: listStatus);
+      }
+    } catch (_) {
+      yield StatusError();
+    }
+  }
+}
+
+// @override
+//   Stream<StatusState> mapEventToState(StatusEvent event) async* {
+//     print("mapEventToState");
+//     yield StatusLoading();
+//     try {
+//       List<Status> listStatus;
+//       if (event is ListStarted) {
+//         listStatus = await statusRepository.fetchStatusByDay(event.user.uid);
+//       }
+//       if (listStatus.length == 0) {
+//         yield StatusEmpty();
+//       } else {
+//         yield StatusLoaded(status: listStatus);
+//       }
+//     } catch (_) {
+//       yield StatusError();
+//     }
+//   }
