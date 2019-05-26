2a3b0270d23a4f9239630475b7b70f5be151f6e8
Exee03 <45579836+Exee03@users.noreply.github.com>
2019-05-26 18:40:10 +0800

    Create status_state.dart

---
+++ lib/status/status_state.dart
@@ -0,0 +1,39 @@
+import 'package:btec_security/models/status.dart';
+import 'package:meta/meta.dart';
+import 'package:equatable/equatable.dart';
+
+@immutable
+abstract class StatusState extends Equatable {
+  StatusState([List props = const []]) : super(props);
+}
+  
+class InitialStatusState extends StatusState {}
+
+class StatusLoading extends StatusState {
+  @override
+  String toString() => 'StatusLoading';
+}
+
+class StatusLoaded extends StatusState {
+  final List<Status> status;
+
+  StatusLoaded({@required this.status}) : assert (status != null);
+
+  @override
+  String toString() => 'StatusLoaded { todos: $status }';
+} 
+
+class StatusNotLoaded extends StatusState {
+  @override
+  String toString() => 'StatusNotLoaded';
+}
+
+class StatusEmpty extends StatusState {
+  @override
+  String toString() => 'StatusEmpty';
+}
+
+class StatusError extends StatusState {
+  @override
+  String toString() => 'StatusError';
+}
