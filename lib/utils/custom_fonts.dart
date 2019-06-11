ae1ddf40da82b65540ccf8fd8bf356a9cf5ab857
Exee03 <45579836+Exee03@users.noreply.github.com>
2019-06-11 20:53:11 +0800

    Update custom_fonts.dart

--- lib/utils/custom_fonts.dart
+++ lib/utils/custom_fonts.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
-class CustomFonts{
-  static const TextStyle appBar = TextStyle(fontSize: 30.0);
+class CustomFonts {
+  static const TextStyle appBar =
+      TextStyle(fontSize: 30.0, color: Colors.white);
+  static const TextStyle invAppBar =
+      TextStyle(fontSize: 30.0, color: Colors.white);
   static const TextStyle invTextStyle = TextStyle(color: Colors.white);
-  static const TextStyle logText = TextStyle(fontSize: 13, color: Colors.white54);
+  static const TextStyle logText =
+      TextStyle(fontSize: 13, color: Colors.white54);
+  static const TextStyle logBigText =
+      TextStyle(fontSize: 25, color: Colors.white54);
 }
