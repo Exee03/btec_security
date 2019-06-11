a1113405cffe5d3ba88347b1429d5a9c6836be96
Exee03 <45579836+Exee03@users.noreply.github.com>
2019-06-11 20:53:59 +0800

    Update history_screen.dart

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -1,5 +1,5 @@
 import 'package:btec_security/data.dart';
-import 'package:btec_security/repository/status_stream.dart';
+import 'package:btec_security/ui/widgets/stream/status_stream.dart';
 import 'package:btec_security/utils/custom_fonts.dart';
 import 'package:flutter/material.dart';
 
@@ -12,15 +12,34 @@ class HistoryScreen extends StatefulWidget {
 }
 
 class _HistoryScreenState extends State<HistoryScreen> {
+  DateTime _dateNow = new DateTime.now();
+  String _date;
+  String _selectedDate;
+  String _labelDate;
+
+  @override
+  void initState() {
+    _date = '${_dateNow.day}-${_dateNow.month}-${_dateNow.year}';
+    _selectedDate = _date;
+    _labelDate = 'Today';
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: widget.menu.colors,
         centerTitle: true,
-        title: Text(
-          widget.menu.title,
-          style: CustomFonts.appBar,
+        title: Hero(
+          tag: widget.menu.title,
+          child: Material(
+            color: Colors.transparent,
+            child: Text(
+              widget.menu.title,
+              style: CustomFonts.appBar,
+            ),
+          ),
         ),
         elevation: 0,
       ),
@@ -29,11 +48,50 @@ class _HistoryScreenState extends State<HistoryScreen> {
           Container(
             color: widget.menu.colors,
             height: MediaQuery.of(context).size.height / 8,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                MaterialButton(
+                  child: ListTile(
+                    leading: Icon(Icons.date_range, color: Colors.white),
+                    title: _labelDate == _date
+                        ? Text(
+                            'Today',
+                            style: CustomFonts.invTextStyle,
+                          )
+                        : Text(
+                            _labelDate,
+                            style: CustomFonts.invTextStyle
+                          ),
+                  ),
+                  onPressed: () async {
+                    final DateTime picked = await showDatePicker(
+                      context: context,
+                      firstDate: new DateTime(2019),
+                      initialDate: _dateNow,
+                      lastDate: new DateTime(2025),
+                    );
+                    if (picked != null && picked != _dateNow) {
+                      setState(() {
+                        _selectedDate =
+                            '${picked.day}-${picked.month}-${picked.year}';
+                        if (_selectedDate == _date) {
+                          _labelDate = 'Today';
+                        } else {
+                          _labelDate = _selectedDate;
+                        }
+                      });
+                    }
+                  },
+                ),
+              ],
+            ),
           ),
           Expanded(
             child: Container(
               padding: EdgeInsets.all(10),
               child: StatusStream(
+                date: _labelDate,
                 uid: widget.uid,
               ),
             ),
