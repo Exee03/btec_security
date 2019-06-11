4ceb3662ff101e9e228ff9b31dfb96446ee6f16e
Exee03 <45579836+Exee03@users.noreply.github.com>
2019-06-11 20:52:55 +0800

    Update storage.py

--- python/storage.py
+++ python/storage.py
@@ -1,5 +1,4 @@
 import pyrebase
-import numpy as np
 import cv2
 
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
@@ -15,9 +14,7 @@ config = {
 }
 
 firebase = pyrebase.initialize_app(config)
-
 storage = firebase.storage()
-#storage.child("image2").put("C:/Users/mohdi/Downloads/Image/DSC_1119.jpg")
 
 while(True):
     #capture frame-by-frame
@@ -32,7 +29,7 @@ while(True):
         #create image and save into project directory
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
-        img_item = "my-image.png"
+        img_item = "image.png"
         cv2.imwrite(img_item, roi_color)
         
         #draw rectangle wit color
@@ -41,6 +38,9 @@ while(True):
         end_cord_x = x + h #ending of coordinate of X
         end_cord_y = y + w #ending of coordinate of Y
         cv2.rectangle(frame, (x,y), (end_cord_x, end_cord_y), color, stroke)
+        
+        #store in Firebase Storage
+        storage.child("image.png").put("C:/Users/mohdi/GitHub/btec_security/python/image.png")
         
     #display resulting frame
     cv2.imshow('frame',frame)
