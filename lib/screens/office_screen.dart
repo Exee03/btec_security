c27793e7297d1467729731b5f57949936fb07684
Exee03 <45579836+Exee03@users.noreply.github.com>
2019-06-11 20:53:55 +0800

    Update office_screen.dart

--- lib/screens/office_screen.dart
+++ lib/screens/office_screen.dart
@@ -1,9 +1,12 @@
 import 'package:btec_security/data.dart';
-import 'package:btec_security/ui/widgets/chart/attendance_chart.dart';
+import 'package:btec_security/employee/employee_screen.dart';
 import 'package:btec_security/utils/custom_colors.dart';
 import 'package:btec_security/utils/custom_fonts.dart';
+import 'package:btec_security/utils/firebase_api.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:btec_security/todos/todo_screen.dart';
 
 class CardModel {
   final Widget icon;
@@ -14,22 +17,29 @@ class CardModel {
 var cardData = <CardModel>[];
 
 class OfficeScreen extends StatefulWidget {
-  OfficeScreen({this.menu});
+  OfficeScreen({this.menu, this.user});
   final Menu menu;
+  final FirebaseUser user;
+
   @override
   _OfficeScreenState createState() => _OfficeScreenState();
 }
 
 class _OfficeScreenState extends State<OfficeScreen> {
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  DateTime _dateNow = new DateTime.now();
+  String _date;
+  int taskToday;
+
   @override
   void initState() {
     super.initState();
     cardData.clear();
+    _date = '${_dateNow.day}-${_dateNow.month}-${_dateNow.year}';
     loadData();
   }
 
   Future loadData() async {
-    cardData.add(CardModel(icon: AttendanceChart(), tiltle: 'Attendance'));
     cardData.add(CardModel(
         icon: LayoutBuilder(builder: (context, constraint) {
           return new Icon(
@@ -50,65 +60,123 @@ class _OfficeScreenState extends State<OfficeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    Widget card(CardModel cardData) {
+    Widget card(CardModel cardData, int index) {
       return Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30.0),
-          ),
-          color: CustomColors.front,
-          child: Container(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Column(
-                children: <Widget>[
-                  Row(
+        child: InkWell(
+          onTap: () async {
+            if (index == 1) {
+              _scaffoldKey.currentState.showSnackBar(
+                SnackBar(
+                  content: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 20.0),
-                        child: Text(
-                          cardData.tiltle,
-                          style: TextStyle(fontSize: 25, color: Colors.white),
-                        ),
+                    children: [
+                      Text(
+                        'Loading...',
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Theme.of(context).primaryColor),
                       ),
-                      Container(
-                        width: 100,
-                        height: 100,
-                        child: cardData.icon,
-                      ),
+                      CircularProgressIndicator(),
                     ],
                   ),
-                  // Padding(
-                  //   padding: const EdgeInsets.only(left: 10.0, bottom: 10),
-                  //   child: Row(
-                  //     mainAxisAlignment: MainAxisAlignment.start,
-                  //     children: <Widget>[],
-                  //   ),
-                  // ),
-                  SizedBox(
-                    height: 1,
-                    width: MediaQuery.of(context).size.width - 50,
-                    child: Container(color: widget.menu.colors),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 8.0, vertical: 5),
-                    child: Row(
+                ),
+              );
+              Map<String, String> container = {
+                'uid': widget.user.uid,
+                'date': _date
+              };
+              taskToday = await getTaskToday(container);
+            }
+
+            Navigator.of(context).push(
+              PageRouteBuilder<Null>(
+                pageBuilder: (
+                  BuildContext context,
+                  Animation<double> animation,
+                  Animation<double> secondaryAnimation,
+                ) {
+                  return AnimatedBuilder(
+                    animation: animation,
+                    builder: (
+                      BuildContext context,
+                      Widget child,
+                    ) {
+                      if (index == 0) {
+                        _scaffoldKey.currentState.hideCurrentSnackBar();
+                        return Opacity(
+                          opacity: animation.value,
+                          child: EmployeeScreen(
+                            user: widget.user,
+                            menu: widget.menu,
+                          ),
+                        );
+                      } else if (index == 1) {
+                        _scaffoldKey.currentState.hideCurrentSnackBar();
+                        return Opacity(
+                          opacity: animation.value,
+                          child: TodoScreen(
+                              user: widget.user,
+                              taskToday: taskToday),
+                        );
+                      }
+                    },
+                  );
+                },
+                transitionDuration: Duration(milliseconds: 500),
+              ),
+            );
+          },
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            color: CustomColors.front,
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  children: <Widget>[
+                    Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.end,
                       children: <Widget>[
-                        Text('VIEW',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 12,
-                                color: widget.menu.colors)),
-                        Icon(Icons.arrow_right, color: widget.menu.colors)
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 20.0),
+                          child: Text(
+                            cardData.tiltle,
+                            style: TextStyle(fontSize: 25, color: Colors.white),
+                          ),
+                        ),
+                        Container(
+                          width: 100,
+                          height: 100,
+                          child: cardData.icon,
+                        ),
                       ],
                     ),
-                  ),
-                ],
+                    SizedBox(
+                      height: 1,
+                      width: MediaQuery.of(context).size.width - 50,
+                      child: Container(color: widget.menu.colors),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8.0, vertical: 5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text('VIEW',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 12,
+                                  color: widget.menu.colors)),
+                          Icon(Icons.arrow_right, color: widget.menu.colors)
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
@@ -117,12 +185,19 @@ class _OfficeScreenState extends State<OfficeScreen> {
     }
 
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         backgroundColor: widget.menu.colors,
         centerTitle: true,
-        title: Text(
-          widget.menu.title,
-          style: CustomFonts.appBar,
+        title: Hero(
+          tag: widget.menu.title,
+          child: Material(
+            color: Colors.transparent,
+            child: Text(
+              widget.menu.title,
+              style: CustomFonts.appBar,
+            ),
+          ),
         ),
         elevation: 0,
       ),
@@ -138,7 +213,7 @@ class _OfficeScreenState extends State<OfficeScreen> {
             child: ListView.builder(
               itemCount: cardData.length,
               itemBuilder: (context, index) {
-                return card(cardData[index]);
+                return card(cardData[index], index);
               },
             ),
           )
