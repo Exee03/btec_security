d6183a85bbb33c47d63863f6b25afdaaa4954f08
Exee03 <45579836+Exee03@users.noreply.github.com>
2019-06-11 20:46:13 +0800

    Add Sidemenu

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+import 'package:btec_security/data.dart';
+import 'package:btec_security/screens/sidemenu/attendance_sidemenu.dart';
+import 'package:btec_security/screens/sidemenu/history_sidemenu.dart';
+import 'package:btec_security/screens/sidemenu/office_sidemenu.dart';
+import 'package:btec_security/screens/sidemenu/profile_sidemenu.dart';
 import 'package:btec_security/utils/custom_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:bloc/bloc.dart';
@@ -8,6 +13,9 @@ import 'package:btec_security/screens/home_screen.dart';
 import 'package:btec_security/login/login_screen.dart';
 import 'package:btec_security/splash.dart';
 import 'package:btec_security/simple_bloc_delegate.dart';
+import 'package:hidden_drawer_menu/hidden_drawer/hidden_drawer_menu.dart';
+import 'package:hidden_drawer_menu/hidden_drawer/screen_hidden_drawer.dart';
+import 'package:hidden_drawer_menu/menu/item_hidden_menu.dart';
 
 main() {
   BlocSupervisor.delegate = SimpleBlocDelegate();
@@ -22,15 +30,20 @@ class _AppState extends State<App> {
   final UserRepository _userRepository = UserRepository();
   AuthenticationBloc _authenticationBloc;
 
+  List<ScreenHiddenDrawer> itens = new List();
+  List<Menu> menu;
+
   ThemeData get _themeData => new ThemeData(
       primaryColor: CustomColors.splashBackground,
-      accentColor: Colors.amberAccent,
+      accentColor: CustomColors.splashBackground,
+      bottomAppBarColor: CustomColors.front,
       scaffoldBackgroundColor: CustomColors.background,
       fontFamily: 'Open_Sans');
 
   @override
   void initState() {
     super.initState();
+    menu = getMenu();
     _authenticationBloc = AuthenticationBloc(userRepository: _userRepository);
     _authenticationBloc.dispatch(AppStarted());
   }
@@ -51,7 +64,71 @@ class _AppState extends State<App> {
               return LoginScreen(userRepository: _userRepository);
             }
             if (state is Authenticated) {
-              return HomeScreen(user: state.user);
+              itens.clear();
+              itens.add(
+                new ScreenHiddenDrawer(
+                  new ItemHiddenMenu(
+                    name: "Home",
+                    colorLineSelected: Theme.of(context).primaryColor,
+                    baseStyle: TextStyle(
+                        color: Colors.white.withOpacity(0.8), fontSize: 25.0),
+                    selectedStyle:
+                        TextStyle(color: Theme.of(context).primaryColor),
+                  ),
+                  HomeScreen(user: state.user, company: state.company),
+                ),
+              );
+              Widget widgetMenu;
+              for (int i = 0; i < menu.length; i++) {
+                if (i == 0) {
+                  widgetMenu = OfficeScreenSidemenu(
+                    menu: menu[i],
+                    user: state.user,
+                  );
+                } else if (i == 1) {
+                  print('state.totalEmployee = ${state.totalEmployee}');
+                  widgetMenu = AttendanceScreenSidemenu(
+                    menu: menu[i],
+                    user: state.user,
+                    totalEmployee: state.totalEmployee,
+                  );
+                } else if (i == 2) {
+                  widgetMenu =
+                      HistoryScreenSidemenu(menu: menu[i], uid: state.user.uid);
+                } else if (i == 3) {
+                  widgetMenu = Profile(
+                    menu: menu[i],
+                    user: state.user,
+                    company: state.company,
+                  );
+                }
+                itens.add(
+                  new ScreenHiddenDrawer(
+                    new ItemHiddenMenu(
+                      name: menu[i].title,
+                      colorLineSelected: menu[i].colors,
+                      baseStyle: TextStyle(
+                          color: Colors.white.withOpacity(0.8), fontSize: 25.0),
+                      selectedStyle: TextStyle(color: menu[i].colors),
+                    ),
+                    widgetMenu,
+                  ),
+                );
+              }
+              return HiddenDrawerMenu(
+                initPositionSelected: 0,
+                screens: itens,
+                backgroundColorMenu: Theme.of(context).scaffoldBackgroundColor,
+                iconMenuAppBar: Icon(
+                  Icons.menu,
+                  color: Colors.white,
+                ),
+                whithAutoTittleName: false,
+                backgroundColorContent:
+                    Theme.of(context).scaffoldBackgroundColor,
+                backgroundColorAppBar: Theme.of(context).bottomAppBarColor,
+                elevationAppBar: 0,
+              );
             }
           },
         ),
